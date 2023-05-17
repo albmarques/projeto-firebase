76bb8cc5dd1249502c810c13e38e07c605b78c52
albmarques <112758494+albmarques@users.noreply.github.com>
2023-05-17 11:17:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const { getFirestore, Timestamp, FieldValue, getDocs} = require('firebase-admin/firestore');
 
 const serviceAccount = require('./projeto-d29d3-firebase-adminsdk-583zz-e25bfcd12f.json');
 
@@ -16,3 +16,16 @@ const data ={
 }
 
 const res = db.collection("projetoweb-test").doc("LA").set(data)
+
+
+const cityRef = db.collection('cities').doc("DC")
+const doc =  cityRef.get();
+
+cityRef.get().then(doc => {
+  if (doc && doc.exists) {
+    console.log(doc.id, '=>', doc.data());
+  }
+})
+.catch(err => {
+  console.log(err);
+});
